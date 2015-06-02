972cba5820edc95cd0b69cdc2063da7f5a3a1062
xdevl <xdevl@users.noreply.github.com>
2015-06-02 23:16:19 +0100

    Initial import

---
+++ script.js
@@ -0,0 +1,153 @@
+var xdevl=xdevl || {} ;
+
+jQuery(function($)
+{		
+	xdevl.CodeSnippet=function(node)
+	{
+		xdevl.CodeSnippet.DEFAULT_LANGUAGE="text" ;
+		xdevl.CodeSnippet.LANGUAGE_PREFIX="language-" ;
+		
+		xdevl.CodeSnippet.isCodeSnippet=function(node)
+		{
+			return node.tagName.toLowerCase()=="code" && node.parentNode!=null
+					&& node.parentNode.tagName.toLowerCase()=="pre" ;
+		} ;
+		
+		this.init=function(node)
+		{
+			this.node=(node && xdevl.CodeSnippet.isCodeSnippet(node))?node:document.createElement("pre").appendChild(document.createElement("code")) ;
+		} ;
+		
+		this.setCode=function(code)
+		{
+			//this.node.textContent=_.escape(code) ;
+			//this.node.innerHTML=prettyPrintOne(_.escape(code)) ;
+			this.node.textContent=code ;
+		} ;
+		
+		this.getCode=function()
+		{
+			return this.node.textContent ;
+		} ;
+		
+		this.setLanguage=function(language)
+		{
+			this.node.className=xdevl.CodeSnippet.LANGUAGE_PREFIX+(language) ;
+			this.node.parentNode.className="prettyprint" ;
+		} ;
+		
+		this.getLanguage=function()
+		{
+			if(!this.node.className || this.node.className.indexOf(xdevl.CodeSnippet.LANGUAGE_PREFIX)!=0)
+				return xdevl.CodeSnippet.DEFAULT_LANGUAGE ;
+			else return this.node.className.substr(xdevl.CodeSnippet.LANGUAGE_PREFIX.length) ;
+		} ;
+		
+		this.isAttached=function()
+		{
+			return this.node.parentNode.parentNode!=null ;
+		} ;
+		
+		this.getHTML=function()
+		{
+			return document.createElement("div").appendChild(this.node.parentNode).parentNode.innerHTML ;
+		} ;
+		
+		this.encode=function(node)
+		{
+			node=node || this.node ;
+			for(child in node.childNodes)
+			{
+				if(child.nodeType==3)
+					child.nodeValue=child.nodeValue.replace(/ /g,"&#32;").replace(/\t/g,"&#9;").replace(/\r?\n/g,"&#10;") ;
+				else if(child.nodeType==1)
+					this.encode(child) ;
+			}
+		} ;
+		
+		this.init(node) ;
+	} ;
+	
+	xdevl.CodeSnippetDialog=wp.media.view.Modal.extend(
+	{
+		controller: { trigger: function() {} },
+		
+		initialize: function()
+		{
+			wp.media.view.Modal.prototype.initialize.apply(this,arguments) ;
+			ModalContentView=wp.Backbone.View.extend({
+				template: wp.template("xdevl-code-snippet"),
+				attributes : {
+					id: "xdevl-code-dialog"
+				}
+			}) ;
+			this.content(new ModalContentView()) ;
+		},
+		
+		open: function()
+		{
+			wp.media.view.Modal.prototype.open.apply(this,arguments) ;
+			if(!this.editor)
+			{
+				this.editor=ace.edit("ace-editor") ;
+				// Diasble error checks
+				//this.editor.getSession().setUseWorker(false);
+				// Hide vertical line hints for line lenght
+				this.editor.setShowPrintMargin(false);
+				
+				var instance=this ;
+				$("#save-code-snippet").click(function() {
+					
+					instance.codeSnippet.setCode(instance.editor.getValue()) ;
+					instance.codeSnippet.setLanguage($("#ace-mode").val()) ;
+					if(!instance.codeSnippet.isAttached())
+						wp.media.editor.insert(instance.codeSnippet.getHTML()) ;
+					
+					instance.close() ;
+					$("#code-snippet-form").ajaxSubmit({});
+				}) ;
+				
+				$("#ace-theme").change(function() {
+					instance.editor.setTheme("ace/theme/"+$("#ace-theme").val()) ;
+				}) ;
+				
+				$("#ace-mode").change(function() {
+					instance.editor.getSession().setMode("ace/mode/"+$("#ace-mode").val()) ;
+				}) ;
+				
+				$("#ace-font-size").change(function() {
+					instance.editor.setFontSize($("#ace-font-size").val()+"em") ;
+				}) ;
+			}
+			
+			var node=tinymce.activeEditor.selection.getNode() ;
+			while(node!=null && !node.tagName.toLowerCase()=="body" && !xdevl.CodeSnippet.isCodeSnippet(node))
+				node=node.parentNode ;
+			
+			this.codeSnippet=new xdevl.CodeSnippet(node) ;
+
+			if(this.codeSnippet.isAttached())
+			{
+				$("#ace-mode").val(this.codeSnippet.getLanguage()) ;
+				$("#ace-mode").change() ;
+			}
+			else this.codeSnippet.setLanguage($("#ace-mode").val()) ;
+			
+			$("#ace-theme").change() ;
+			$("#ace-font-size").change() ;
+			
+			this.editor.setValue(this.codeSnippet.getCode(),1) ;
+		}
+	}) ;
+	
+	xdevl.codeSnippetDialog=new xdevl.CodeSnippetDialog() ;
+	
+	$(document).ready
+	(
+		function()
+		{
+			$("#xdevl-code-snippet").click(function(){xdevl.codeSnippetDialog.open();}) ;
+		}
+	) ;
+}) ;
+
---
+++ xdevl-code-snippet.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * Plugin Name: XdevL code snippet
+ * Plugin URI: http://www.xdevl.com/blog
+ * Description: Create and manage inline code snippets in your posts
+ * Version: 1.0
+ * Author: XdevL
+ * Author URI: http://www.xdevl.com/blog
+ * License: GPL2
+ *
+ * @copyright Copyright (c) 2015, XdevL
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace xdevl\codesnippet
+{
+
+defined( 'ABSPATH' ) or die('No script kiddies please!') ;
+class Constant
+{
+	// Theme settings
+	const THEME_SETTINGS='xdevl:codesnippet:theme' ;
+	const THEME_SETTINGS_NAME=self::THEME_SETTINGS.':name' ;
+	const THEME_SETTINGS_DEFAULT_NAME='default' ;
+	const THEME_SETTINGS_FONT_SIZE=self::THEME_SETTINGS.':font_size' ;
+	const THEME_SETTINGS_DEFAULT_FONT_SIZE='1' ;
+
+	// Editor settings
+	const EDITOR_SETTINGS='xdevl:codesnippet:editor' ;
+	const EDITOR_SETTINGS_LANGUAGE=self::EDITOR_SETTINGS.':language' ;
+	const EDITOR_SETTINGS_DEFAULT_LANGUAGE='c_cpp' ;
+	const EDITOR_SETTINGS_THEME=self::EDITOR_SETTINGS.':theme' ;
+	const EDITOR_SETTINGS_DEFAULT_THEME='github' ;
+	const EDITOR_SETTINGS_FONT_SIZE=self::EDITOR_SETTINGS.':font_size' ;
+	const EDITOR_SETTINGS_DEFAULT_FONT_SIZE='1rem' ;
+}
+
+
+function echo_files_as_options($dirName, $suffix, $prefix, $value)
+{
+	if($directory=opendir(plugin_dir_path(__FILE__).$dirName))
+	{
+		$options=array() ;
+		while(($file=readdir($directory))!==false)
+		{
+			if((empty($suffix) || strpos($file,$suffix)===0) && (empty($prefix) || strpos($file,$prefix)===strlen($file)-strlen($prefix)))
+				array_push($options,substr($file,strlen($suffix),strlen($prefix)*-1)) ;
+		}
+		closedir($directory) ;
+		sort($options,SORT_STRING) ;
+		
+		foreach($options as $option)
+			if($option==$value)
+				echo "<option value=\"$option\" selected=selected>$option</option>" ;
+			else echo "<option value=\"$option\">$option</option>" ;
+	}
+}
+
+function echo_ace_options($type, $value)
+{
+	echo_files_as_options('ace',$type.'-','.js',$value) ;
+}
+
+function echo_font_size_options($value)
+{
+	for($i=5;$i<=15;++$i)
+	{
+		$font_size=$i/10 ;
+		if($font_size==$value)
+			echo "<option value=\"$font_size\" selected=selected>$font_size em</option>" ;
+		else echo "<option value=\"$font_size\">$font_size em</option>" ;
+	}
+}
+
+function echo_prettify_options($value)
+{
+	if(empty($value) || $value=='default')
+		echo "<option value=\"default\" selected=selected>default</option>" ;
+	else echo "<option value=\"default\">default</option>" ;
+	echo_files_as_options('google-code-prettify/themes','','.css',$value) ;
+}
+
+function media_buttons()
+{
+	echo '<a href="#" id="xdevl-code-snippet" class="button"><span class="xdevl-code-button"></span>Code</a>' ;
+}
+
+function wp_enqueue_media()
+{
+	wp_register_style('xdevl-code-snippet-style',plugins_url('style.css',__FILE__)) ;
+	wp_enqueue_style('xdevl-code-snippet-style') ;
+	
+	wp_register_script('ace',plugins_url('ace/ace.js',__FILE__),array( 'jquery' )) ;
+	wp_enqueue_script('ace') ;
+	
+	// TODO: use wp_localize_script to pass default parameters
+	wp_register_script('xdevl-code-snippet-script',plugins_url('script.js',__FILE__),array('jquery','jquery-form','ace')) ;
+	wp_enqueue_script('xdevl-code-snippet-script') ;
+}
+
+function admin_footer()
+{
+	?>
+<script type="text/template" id="tmpl-xdevl-code-snippet">
+	<div class="wrapper">
+		<div class="header">
+			<h1>Code snippet</h1>
+			<form id="code-snippet-form" method="post" action="options.php">
+				<?php settings_fields(Constant::EDITOR_SETTINGS);
+					do_settings_sections(Constant::EDITOR_SETTINGS); ?>
+				
+				<div class="field">					
+				<label for="<?php echo Constant::EDITOR_SETTINGS_LANGUAGE; ?>">Language:</label><select id="ace-mode" name="<?php echo Constant::EDITOR_SETTINGS_LANGUAGE; ?>">
+						<?php echo_ace_options('mode',get_option(Constant::EDITOR_SETTINGS_LANGUAGE,Constant::EDITOR_SETTINGS_DEFAULT_LANGUAGE)); ?></select>
+				</div>
+				<div class="field">
+				<label for="<?php echo Constant::EDITOR_SETTINGS_FONT_SIZE; ?>">Font size:</label><select id="ace-font-size" name="<?php echo Constant::EDITOR_SETTINGS_FONT_SIZE; ?>">
+						<?php echo_font_size_options(get_option(Constant::EDITOR_SETTINGS_FONT_SIZE,Constant::EDITOR_SETTINGS_DEFAULT_FONT_SIZE)); ?></select>
+				</div>
+				<div class="field">		
+				<label for="<?php echo Constant::EDITOR_SETTINGS_THEME; ?>">Theme:</label><select id="ace-theme" name="<?php echo Constant::EDITOR_SETTINGS_THEME; ?>"
+						<?php echo_ace_options('theme',get_option(Constant::EDITOR_SETTINGS_THEME,Constant::EDITOR_SETTINGS_DEFAULT_THEME)); ?></select>
+				</div>
+				<div class="field">
+					<a href="#" id="save-code-snippet" class="button-primary"><?php _e( 'Save snippet' ); ?></a>
+				</div>
+			</form>
+		</div>
+		<div class="editor"><div id="ace-editor"></div></div>
+	</div>
+</script>
+	<?php
+}
+
+function code_snippet_page()
+{
+	?>
+<div class="wrap">
+	<h2>XdevL code snippets setup</h2>
+	<form method="post" action="options.php">
+		<?php settings_fields(Constant::THEME_SETTINGS);
+			do_settings_sections(Constant::THEME_SETTINGS); ?>
+			
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row"><label for="<?php echo Constant::THEME_SETTINGS_NAME; ?>">Theme:</label></th>
+						<td><select name="<?php echo Constant::THEME_SETTINGS_NAME; ?>">
+							<?php echo_prettify_options(get_option(Constant::THEME_SETTINGS_NAME,Constant::THEME_SETTINGS_DEFAULT_NAME)); ?></select></td>
+					</tr>
+					<tr>
+						<th scope="row"><label for="<?php echo Constant::THEME_SETTINGS_FONT_SIZE; ?>">Font size:</label></th>
+						<td><select name="<?php echo Constant::THEME_SETTINGS_FONT_SIZE; ?>">
+							<?php echo_font_size_options(get_option(Constant::THEME_SETTINGS_FONT_SIZE,Constant::THEME_SETTINGS_DEFAULT_FONT_SIZE)); ?></select></td>
+					</tr>
+				</tbody>
+			</table>
+		
+		<?php submit_button(); ?>
+	</form>
+</div>
+	<?php
+}
+
+function admin_menu()
+{
+	add_theme_page('XdevL code snippets setup', 'XdevL code snippets', 'edit_theme_options', 'xdevl-code-snippet', __NAMESPACE__.'\code_snippet_page') ;
+}
+
+function wp_enqueue_scripts()
+{
+	$theme=get_option(Constant::THEME_SETTINGS_NAME,Constant::THEME_SETTINGS_DEFAULT_NAME) ;
+	if(empty($theme) || $theme=="default")
+		$themeFile='google-code-prettify/prettify.css' ;
+	else $themeFile="themes/$theme.css" ;
+	
+	wp_register_style('prettify-css',plugins_url($themeFile,__FILE__)) ;
+	wp_enqueue_style('prettify-css') ;
+	
+	wp_register_script('prettify-script',plugins_url('google-code-prettify/prettify.js',__FILE__),array('jquery')) ;
+	wp_enqueue_script('prettify-script') ;
+}
+
+function wp_head()
+{
+	?>
+	<style>pre.prettyprint {font-size: <?php echo esc_attr(get_option(
+		Constant::THEME_SETTINGS_FONT_SIZE,Constant::THEME_SETTINGS_DEFAULT_FONT_SIZE)); ?>em !important;} </style>
+	
+	<?php
+}
+
+function wp_footer()
+{
+	?>
+		<script type="text/javascript">prettyPrint();</script>
+	<?php
+}
+
+function admin_init()
+{
+	register_setting(Constant::THEME_SETTINGS,Constant::THEME_SETTINGS_NAME) ;
+	register_setting(Constant::THEME_SETTINGS,Constant::THEME_SETTINGS_FONT_SIZE) ;
+	register_setting(Constant::EDITOR_SETTINGS,Constant::EDITOR_SETTINGS_LANGUAGE) ;
+	register_setting(Constant::EDITOR_SETTINGS,Constant::EDITOR_SETTINGS_FONT_SIZE) ;
+	register_setting(Constant::EDITOR_SETTINGS,Constant::EDITOR_SETTINGS_THEME) ;
+}
+
+// admin setup
+if(is_admin())
+{
+	add_action('media_buttons',__NAMESPACE__.'\media_buttons',20) ;
+	add_action('wp_enqueue_media',__NAMESPACE__.'\wp_enqueue_media') ;
+	add_action('admin_footer-post.php',__NAMESPACE__.'\admin_footer') ;
+	add_action('admin_footer-post-new.php',__NAMESPACE__.'\admin_footer') ;
+	add_action('admin_menu',__NAMESPACE__.'\admin_menu') ;
+	add_action('admin_init',__NAMESPACE__.'\admin_init') ;
+}
+// front end setup
+else
+{
+	add_action('wp_enqueue_scripts',__NAMESPACE__.'\wp_enqueue_scripts') ;
+	add_action('wp_head',__NAMESPACE__.'\wp_head') ;
+	add_action('wp_footer',__NAMESPACE__.'\wp_footer') ;
+}
+
+} //Endo of xdevl\codesnippet
+
+?>
